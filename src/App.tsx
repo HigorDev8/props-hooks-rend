6954213551d807ac4cb6754c149cee4eb785e143
HigorDev8 <higordev8@gmail.com>
2024-05-14 08:37:33 -0300

    props-hooks-rend

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import Card from "./componentes/Card/Card"
+import Contador from "./componentes/Contador/Contador"
+import Task from "./componentes/Task/Task"
+
+function App() {
+  return (
+    <>
+      {/* <Card titulo="React" descricao= "Biblioteca com JS" />
+
+      <Card titulo="Spring" descricao= "Framework de Java" /> */
+      
+      }
+      
+    {/* <Contador /> */}
+
+    <Task />
+    </>
+  )
+}
+
+export default App
